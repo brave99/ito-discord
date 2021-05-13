1010a285495fa1a5e96fabee7341c41e4e98a6dc
brave99 <39230206+brave99@users.noreply.github.com>
2021-05-14 00:04:11 +0900

    Add files via upload
    tested up to 2 cards

--- run_bot.py
+++ run_bot.py
@@ -12,18 +12,22 @@ Have fun with your BOT!!
 
 #===== modules =====#
 import discord
-from time import sleep
 from threading import Thread
 from queue import Queue
 import configparser
 import random
 
 #===== global =====#
+### discord ###
 config = configparser.ConfigParser()
 config.read('option.ini', encoding = 'utf8')
 client = discord.Client()
 GAME = discord.Game(name = "ito")
 CHANNEL = None#discord.channel(id=config["BOT"]["CHANNEL"])
+
+### game ###
+players = []
+cards = 1
 STARTED = False
 PLAYING = 0 #0: not playing, 1: kumonoito, 2: akaiito
 sendq = Queue() #need this when the game send message to discord
@@ -48,7 +52,9 @@ async def on_message(message):
     global PLAYING
     global CHANNEL
     global validate
-    players = []
+    global players
+    global cards
+
     if message.content.startswith("/shutdown"):
         if client.user != message.author:
             await message.channel.send("Bye!!")
@@ -61,8 +67,8 @@ async def on_message(message):
             if client.user != message.author:
                 await CHANNEL.send("クモノイトを始めます。")
                 await client.change_presence(activity = GAME)
-                await CHANNEL.send('参加したい人は"/join"と入力。')
-                await CHANNEL.send('全員の入力が終わったら"/go"と入力。')
+                await CHANNEL.send('参加したい人は"/join"と入力。\n全員の入力が終わったら"/go"と入力。')
+                await CHANNEL.send('配られる数字の数は"/cards 2"のように変更できます。(初期値は1)')
                 STARTED = True
 
     elif PLAYING == 0 and STARTED:
@@ -77,6 +83,13 @@ async def on_message(message):
                     hoge = Player(message.author)
                     players.append(hoge)
                     await CHANNEL.send("{} を登録しました。".format(message.author.name))
+        
+        elif message.content.startswith("/cards"):
+            try:
+                cards = int(message.content.split(" ")[1])
+                await CHANNEL.send(str(cards)+"枚ずつ配ります。")
+            except:
+                await CHANNEL.send("入力が正しくありません。")
 
         elif message.content.startswith("/go"):
             if len(players)<2:
@@ -87,24 +100,25 @@ async def on_message(message):
 
                 ### deal number ###
                 await CHANNEL.send("全員の準備が完了しました。DMでカードを配布します。")
-                deal = dealNumber(num_player)
+                deal,ans = dealNumber(num_player, cards)
                 for i, player in enumerate(players):
                     player.number = deal[i]
+                    player.number_hard = player.number[:]
 
                 for player in players: 
                     #player.notice()#this does not work
                     await player.discord.send('{0} さんの数字は【{1}】です。'.format(player.name, player.number))
 
-                await CHANNEL.send('話し合いを始めてください。\n数字を公開する人は順番に"/open"と入力。')
+                await CHANNEL.send('お題を決めて話し合いを始めてください。\n数字を公開する人は順番に"/open"と入力。')
                 
-                deal.sort()
                 failed = False
-                for num in deal:### TODO: make this area clear.
+                for num in ans:### TODO: make this area clear.
                     message = await client.wait_for('message', check=wait_for_open)
                     for player in players:
                         if message.author == player.discord:
-                            await CHANNEL.send(player.name+"の数字は【"+str(player.number)+"】です。")
-                            if player.number == num:
+                            challenge = player.number[0]
+                            await CHANNEL.send(player.name+"の数字は【"+str(player.number.pop(0))+"】です。")
+                            if challenge == num:
                                 await CHANNEL.send("セーフ")
                             else:
                                 await CHANNEL.send("アウトォォォ！！！")
@@ -121,17 +135,18 @@ async def on_message(message):
                             break
                     if failed:
                         break
-                if not failed:
+                if not failed: # cleared
                     await CHANNEL.send("クリアおめでとう！\n各プレイヤーの数字は以下の通りです。")
                     content = "{0}:\t{1}"
                     for player in players:
-                        await CHANNEL.send(content.format(player.name,player.number))
+                        await CHANNEL.send(content.format(player.name,player.number_hard))
                 else:
                     await CHANNEL.send("残念だったね。")
 
                 STARTED = False
                 PLAYING = 0
                 players = []
+                cards = 1
                 await CHANNEL.send('"/start" で次のゲームを開始します。\n"/shutdown"で終了します。')
 
 #===== supporting functions =====#
@@ -144,21 +159,28 @@ def wait_for_player(message):
 #===== itoGame =====#
 class Player():
     def __init__(self, discord):
-        self.role = ""
-        self.type = ""
         self.discord = discord
         self.name = self.discord.name
-        self.number = 0
+        self.number = []
+        self.number_hard = []
 
-    def notice(self):
+    def notice(self):# this does not work
         self.discord.send(self.name+"さんの数字は【"+str(self.number)+"】です。")
 
 
-def dealNumber(num_player):
-    num_player = int(num_player)
-    rand = [i+1 for i in range(100)] # 1~100
-    deal = random.sample(rand,num_player)
-    return deal
+def dealNumber(num_player, cards):
+    rand_num = num_player * cards
+    base = [i+1 for i in range(100)] # 1~100
+    deal = []
+    ans = random.sample(base,rand_num)
+    for i in range(num_player):
+        tmp = []
+        for j in range(cards):
+            tmp.append(ans[cards*i+j])
+        tmp.sort()
+        deal.append(tmp)
+    ans.sort()
+    return deal, ans
 
 
 def gameFailure(players):
@@ -166,7 +188,7 @@ def gameFailure(players):
     sendq.put("失敗です。\n各プレイヤーの数字は以下の通りです。")
     content = "{0}:\t{1}"
     for player in players:
-        sendq.put(content.format(player.name,player.number))
+        sendq.put(content.format(player.name,player.number_hard))
     sendq.put("end")
 
 
